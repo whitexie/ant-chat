69a77d7a80499adb50dcb4f1736a5b610b675956
Ysansan <ysansan98@outlook.com>
2025-01-15 21:43:15 +0800

    chore: 删除 react-query 依赖

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,15 @@
 import { ThemeProvider } from '@/contexts/theme'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import App from './App.tsx'
 import 'uno.css'
 import './index.css'
 
-const queryClient = new QueryClient()
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
+import type { PluginOption } from 'vite'
+import process from 'node:process'
 import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
+
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
+
+const plugins: PluginOption[] = [
+  react(),
+  UnoCSS(),
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(analyzer())
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    UnoCSS(),
-  ],
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
