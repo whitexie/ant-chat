df9a5e3b56677dc5a20f635e3d5f6d07c1b4c363
Ysansan <ysansan98@outlook.com>
2025-01-21 22:16:24 +0800

    refactor: 从`Settings` 组件中抽离 `SideButton` 组件

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,5 +1,6 @@
 import { SettingOutlined } from '@ant-design/icons'
 import { lazy, useState } from 'react'
+import { SideButton } from '../SideButton'
 
 const SettingsModal = lazy(() => import('./Modal'))
 
@@ -8,14 +9,12 @@ export default function Settings() {
 
   return (
     <>
-      <div className="flex w-full items-center gap-4 cursor-pointer hover:bg-gray-300 p-2 rounded-md" onClick={() => setOpen(true)}>
-        <div className="flex-shrink-0 flex justify-center items-center">
-          <SettingOutlined className="w-4 h-4" />
-        </div>
-        <div className="flex-1">
-          设置
-        </div>
-      </div>
+      <SideButton
+        icon={<SettingOutlined className="w-4 h-4" />}
+        onClick={() => setOpen(true)}
+      >
+        设置
+      </SideButton>
       <SettingsModal
         open={open}
         onClose={() => setOpen(false)}
