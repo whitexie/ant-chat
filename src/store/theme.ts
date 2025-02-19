350a86cbbaad43c9c48ccfdebe3d49c3e29fe28a
ysansan <hippeauxie@gmail.com>
2025-02-19 13:40:21 +0800

    fix: 优化黑暗模式mask背景适配

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -9,6 +9,7 @@ export const useThemeStore = create<ThemeStore>()((set, get) => ({
   theme: 'default',
   toggleTheme: (theme) => {
     if (!theme) {
+      document.documentElement.classList.toggle('dark', get().theme === 'default')
       set({ theme: get().theme === 'default' ? 'dark' : 'default' })
     }
     else {
