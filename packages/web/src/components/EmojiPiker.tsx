e609ff7852a632dafc99668f6d0b92baa369766a
ysansan <hippeauxie@gmail.com>
2025-05-06 15:30:56 +0800

    fix: 修复popover第二次显示时触发onEmojiSelect事件

--- packages/web/src/components/EmojiPiker.tsx
+++ packages/web/src/components/EmojiPiker.tsx
@@ -10,6 +10,7 @@ export interface EmojiPickerProps {
 export function EmojiPickerHoc({ value, onChange }: EmojiPickerProps) {
   const [open, setOpen] = React.useState(false)
   function handleEmojiSelect(e: string) {
+    // console.log('handleEmojiSelect => ', e)
     onChange?.(e)
     setOpen(false)
   }
@@ -19,6 +20,8 @@ export function EmojiPickerHoc({ value, onChange }: EmojiPickerProps) {
       open={open}
       trigger="click"
       placement="rightTop"
+      destroyTooltipOnHide
+      styles={{ body: { padding: 0 } }}
       content={(
         <div className="">
           <EmojiPicker className="border-black/10 dark:border-white/20" onEmojiSelect={handleEmojiSelect}>
@@ -31,56 +34,14 @@ export function EmojiPickerHoc({ value, onChange }: EmojiPickerProps) {
           </EmojiPicker>
         </div>
       )}
-      onOpenChange={(open) => {
-        setOpen(open)
-      }}
+      onOpenChange={value => setOpen(value)}
     >
-      <Button icon={value} onClick={() => setOpen(true)} />
+      <Button
+        icon={<span>{value}</span>}
+        onClick={(e) => {
+          e.preventDefault()
+        }}
+      />
     </Popover>
   )
 }
-
-// export function EmojiPicker({ value, onChange }: EmojiPickerProps) {
-//   // const theme = useThemeStore(state => state.theme)
-//   const [open, setOpen] = React.useState(false)
-//   return (
-//     <Popover
-//       open={open}
-//       trigger="click"
-//       placement="rightTop"
-//       content={(
-//         <_EmojiPicker
-//           theme={Theme.AUTO}
-//           categories={[
-//             { category: Categories.ANIMALS_NATURE, name: 'animals-nature' },
-//             { category: Categories.ACTIVITIES, name: 'activities' },
-//             { category: Categories.FOOD_DRINK, name: 'food-drink' },
-//             { category: Categories.TRAVEL_PLACES, name: 'travel-places' },
-//             { category: Categories.OBJECTS, name: 'objects' },
-//             { category: Categories.SYMBOLS, name: 'symbols' },
-//             { category: Categories.FLAGS, name: 'flags' },
-//           ]}
-//           searchDisabled={false}
-//           skinTonesDisabled={true}
-//           lazyLoadEmojis={true}
-//           previewConfig={{
-//             showPreview: false,
-//           }}
-//           onEmojiClick={(e) => {
-//             console.log('click emoji => ', e)
-//             onChange?.(e.emoji)
-//             setOpen(false)
-//           }}
-//         />
-//       )}
-//     >
-//       <Button onClick={() => {
-//         setOpen(true)
-//       }}
-//       >
-//         {value}
-//       </Button>
-//     </Popover>
-
-//   )
-// }
