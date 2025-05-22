ae6b72cb1bbb318a7be7a5f60855f98671dfc347
ysansan <hippeauxie@gmail.com>
2025-05-22 10:05:21 +0800

    fix(electron): 修复初始化数据不匹配问题

--- packages/electron/src/main/db/initializeData.ts
+++ packages/electron/src/main/db/initializeData.ts
@@ -14,17 +14,17 @@ const providerServiceModelsData = [
   { id: 'o1-preview-2024-09-12', model: 'o1-preview-2024-09-12', modelFeatures: { functionCall: true, deepThinking: true, vision: false }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
 
   // ============================ DeepSeek 内置模型 ============================
-  { id: 'deepseek-chat', model: 'deepseek-chat', modelFeatures: { functionCall: true, deepThinking: false, vision: false }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'deepseek-reasoner', model: 'deepseek-reasoner', modelFeatures: { functionCall: false, deepThinking: true, vision: false }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
+  { id: 'deepseek-chat', model: 'deepseek-chat', modelFeatures: { functionCall: true, deepThinking: false, vision: false }, isBuiltin: true, isEnabled: true, providerServiceId: 'deepseek' },
+  { id: 'deepseek-reasoner', model: 'deepseek-reasoner', modelFeatures: { functionCall: false, deepThinking: true, vision: false }, isBuiltin: true, isEnabled: true, providerServiceId: 'deepseek' },
 
   // ============================ Gemini 内置模型 ============================
-  { id: 'gemini-2.5-flash-preview-0417', model: 'Gemini 2.5 Flash Preview 0417', modelFeatures: { functionCall: true, deepThinking: true, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'gemini-2.5-pro-preview-05-06', model: 'Gemini 2.5 Pro Preview 05-06', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'gemini 2.5-pro-exp-03-25', model: 'Gemini 2.5 Pro Exp 03-25', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'gemini-2.0-flash', model: 'Gemini 2.0 Flash', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'gemini-2.0-flash-lite', model: 'Gemini 2.0 Flash-Lite', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'gemini-1.5-flash', model: 'Gemini 1.5 Flash', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
-  { id: 'gemini-1.5-pro', model: 'Gemini 1.5 Pro', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'openai' },
+  { id: 'gemini-2.5-flash-preview-0417', model: 'Gemini 2.5 Flash Preview 0417', modelFeatures: { functionCall: true, deepThinking: true, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
+  { id: 'gemini-2.5-pro-preview-05-06', model: 'Gemini 2.5 Pro Preview 05-06', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
+  { id: 'gemini 2.5-pro-exp-03-25', model: 'Gemini 2.5 Pro Exp 03-25', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
+  { id: 'gemini-2.0-flash', model: 'Gemini 2.0 Flash', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
+  { id: 'gemini-2.0-flash-lite', model: 'Gemini 2.0 Flash-Lite', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
+  { id: 'gemini-1.5-flash', model: 'Gemini 1.5 Flash', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
+  { id: 'gemini-1.5-pro', model: 'Gemini 1.5 Pro', modelFeatures: { functionCall: true, deepThinking: false, vision: true }, isBuiltin: true, isEnabled: true, providerServiceId: 'gemini' },
 
 ]
 
