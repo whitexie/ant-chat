7d5828d872c47ee9ca6f9a4e56e89fc587ec9ab1
ysansan <hippeauxie@gmail.com>
2025-05-23 23:05:54 +0800

    chore(electron): 更新依赖和引入react-scan
    - 将 react-markdown 版本更新至 10.0.0，支持新特性。
    - 新增 react-scan 依赖，集成扫描功能以增强开发体验。
    - 在主渲染文件中添加开发环境下的扫描逻辑，提升调试效率。

--- packages/electron/src/renderer/src/main.tsx
+++ packages/electron/src/renderer/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
+import { scan } from 'react-scan'
 
 import router from './routers/index.tsx'
 import '@ant-design/v5-patch-for-react-19'
 import './index.css'
+
+if(import.meta.env.DEV) {
+  scan({
+    enabled: true,
+  })
+}
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
