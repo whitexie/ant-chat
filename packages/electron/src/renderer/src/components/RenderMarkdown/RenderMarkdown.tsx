41540eb9493e17b4b6b2ca0e65576c672f5b611f
ysansan <hippeauxie@gmail.com>
2025-05-23 22:59:36 +0800

    fix(electron): 优化 RenderMarkdown 组件
    - 引入 useMemo 钩子，重构 remarkPlugins 和 components 的定义，提升性能。
    - 创建 defaultLangPlugin 插件，自动为无语言标记的代码块设置默认语言为 plaintext。
    - 更新代码块渲染逻辑，确保在不同主题下的样式一致性。

--- packages/electron/src/renderer/src/components/RenderMarkdown/RenderMarkdown.tsx
+++ packages/electron/src/renderer/src/components/RenderMarkdown/RenderMarkdown.tsx
@@ -1,8 +1,9 @@
-import { useThemeStore } from '@/store/theme'
 import { Typography } from 'antd'
+import { useMemo } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { visit } from 'unist-util-visit'
+import { useThemeStore } from '@/store/theme'
 import CodeBlock from './CodeBlock'
 import style from './style.module.scss'
 
@@ -10,49 +11,48 @@ export interface RenderMarkdownProps {
   content: string
 }
 
+// 创建默认语言插件
+function defaultLangPlugin() {
+  return (tree: any) => {
+    visit(tree, 'code', (node: any) => {
+      node.lang = node.lang ?? 'plaintext'
+    })
+  }
+}
+
 export default function RenderMarkdown({ content }: RenderMarkdownProps) {
   const theme = useThemeStore(state => state.theme)
+
+  const remarkPlugins = useMemo(() => [
+    defaultLangPlugin,
+    remarkGfm,
+  ], [])
+
+  const components = useMemo(() => ({
+    code: (props: any) => {
+      const { children, className } = props
+      const language = className ? className.replace('language-', '') : 'txt'
+
+      return className
+        ? (
+            <CodeBlock language={language} theme={theme}>
+              {children}
+            </CodeBlock>
+          )
+        : (
+            <code>
+              {children}
+            </code>
+          )
+    },
+  }), [theme])
+
 
   return (
     <Typography className={style['markdown-typography']}>
       <ReactMarkdown
-        remarkPlugins={[
-          /**
-           * 9.0.0版本以后 code函数中props没有`inline`属性，
-           * 因此需通过插件来给下面这种代码块配置一个默认的语法这里使用`plaintext`
-           * ```
-           * ```
-           * 这种没有标记语言的代码块
-           * @see https://github.com/orgs/remarkjs/discussions/1346
-           *
-           * react-markdown changelog:
-           * @see https://github.com/remarkjs/react-markdown/blob/main/changelog.md#remove-extra-props-passed-to-certain-components
-           */
-          () => (tree) => {
-            visit(tree, 'code', (node) => {
-              node.lang = node.lang ?? 'plaintext'
-            })
-          },
-          remarkGfm,
-        ]}
-        components={{
-          code: (props) => {
-            const { children, className } = props
-            const language = className ? className.replace('language-', '') : 'txt'
-
-            return className
-              ? (
-                  <CodeBlock language={language} theme={theme}>
-                    {children}
-                  </CodeBlock>
-                )
-              : (
-                  <code>
-                    {children}
-                  </code>
-                )
-          },
-        }}
+        remarkPlugins={remarkPlugins}
+        components={components}
         urlTransform={url => url}
       >
         {content}
