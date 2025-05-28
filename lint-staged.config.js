4778a54ec652b757f9b6cbf96f51d6255560a391
ysansan <hippeauxie@gmail.com>
2025-05-28 11:16:33 +0800

    chore: 更新 lint-staged 配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  'packages/web/**/*.ts?(x)': () => 'tsc -p packages/web/tsconfig.json --noEmit',
-  'src/**/*.{ts,js,jsx,tsx,vue}': 'eslint --fix',
+  'packages/electron/**/*.ts?(x)': () => 'tsc -p packages/electron/tsconfig.json --noEmit',
+  'packages/**/*.{ts,js,jsx,tsx,vue}': 'eslint --fix',
 }
