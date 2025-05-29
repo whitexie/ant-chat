18faddcf0427dee82e5bb26638e5560fba654380
ysansan <hippeauxie@gmail.com>
2025-05-29 10:33:04 +0800

    feat(chatSettings): 支持状态持久化

--- packages/electron/src/renderer/src/store/chatSettings/store.ts
+++ packages/electron/src/renderer/src/store/chatSettings/store.ts
@@ -1,5 +1,6 @@
 import type { AllAvailableModelsSchema } from '@ant-chat/shared'
 import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
 
 const initializeState = {
   model: null as AllAvailableModelsSchema['models'][number] | null,
@@ -8,6 +9,12 @@ const initializeState = {
   maxTokens: 4000,
 }
 
-export const useChatSttings = create<typeof initializeState>()(() => ({
-  ...initializeState,
-}))
+export const useChatSttings = create<typeof initializeState>()(
+  devtools(
+    persist(
+      () => ({ ...initializeState }),
+      { name: 'chat-settings' },
+    ),
+    { enabled: import.meta.env.MODE === 'development' },
+  ),
+)
