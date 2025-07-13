5fd86c875d479aead1067ce6154052f0f2837ac6
ysansan <hippeauxie@gmail.com>
2025-07-13 14:15:15 +0800

    refactor(electron): 抽离react devtools安装逻辑

--- packages/electron/src/main/index.ts
+++ packages/electron/src/main/index.ts
@@ -1,10 +1,10 @@
 import process from 'node:process'
 import { app } from 'electron'
-import { installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer'
 import { initializeDb } from './db'
 import { registerIpcEvents } from './ipcHandlers'
 import { registerMcpHandlers } from './ipcHandlers/mcpHandlers'
 import { clientHub } from './mcpClientHub'
+import { installDevTools } from './plugins/devtools'
 import { isDev } from './utils/env'
 import { logger } from './utils/logger'
 import { MainWindow } from './window'
@@ -18,30 +18,7 @@ app.whenReady().then(async () => {
 
   // 安装开发工具扩展
   if (isDev) {
-    try {
-      // 先安装 React Developer Tools
-      await installExtension(REACT_DEVELOPER_TOOLS, {
-        loadExtensionOptions: {
-          allowFileAccess: true,
-        },
-        forceDownload: true,
-      })
-        .then(ext => logger.info(`Added Extension: ${ext.name}`))
-        .catch(err => logger.error('React DevTools installation error:', err))
-
-      // 再安装 Redux DevTools
-      await installExtension(REDUX_DEVTOOLS, {
-        loadExtensionOptions: {
-          allowFileAccess: true,
-        },
-        forceDownload: true,
-      })
-        .then(ext => logger.info(`Added Extension: ${ext.name}`))
-        .catch(err => logger.error('Redux DevTools installation error:', err))
-    }
-    catch (err) {
-      logger.error('Failed to install dev tools:', err)
-    }
+    installDevTools()
   }
 
   // 初始化数据库
