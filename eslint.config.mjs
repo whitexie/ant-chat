5992aa321f9ffcd2e0c2f001376bf085114da8aa
ysansan <hippeauxie@gmail.com>
2025-07-18 14:47:48 +0800

    chore: 引入eslint-plugin-better-tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
 export default antfu(
   {
@@ -19,9 +20,29 @@ export default antfu(
   },
   {
     files: ['packages/electron/**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
     rules: {
+      // enable all recommended rules to report a warning
+      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+      // enable all recommended rules to report an error
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+
+      // or configure rules individually
+      'better-tailwindcss/enforce-consistent-line-wrapping': ['warn', { printWidth: 100 }],
       'ts/no-require-imports': ['off'],
       'node/prefer-global/process': ['off'],
+    },
+    settings: {
+      entryPoint: './packages/electron/src/renderer/src/index.css',
     },
   },
   {
